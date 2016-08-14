ceea363a3e51d83126e7a3052ecc025ee2d1d9d5
Helder <heldrida@gmail.com>
2016-08-14 20:55:09 +0100

    installed axios to handle the http get requests; created the url const for the weather api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,17 @@
+import axios from 'axios';
 import api_keys from '../../api_keys';
 
 const API_KEY = api_keys.weather_api;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},uk`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
